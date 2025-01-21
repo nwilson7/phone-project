4353dcfdb0c2a39513b6f31f163be8f613fb2a91
mirunaaf <filfan.miruna@gmsil.com>
2025-01-21 13:47:17 +0000

    update contact bug fix

--- updateContacts.sh
+++ updateContacts.sh
@@ -22,34 +22,34 @@ update_record()
         case $choice in
 
             1)
-                current_name=$(echo "$contact" | cut -d ',' -f 2)
-                current_number=$(echo "$contact" | cut -d ',' -f 3)
+                current_name=$(echo "$contact" | cut -d ',' -f 1)
+                current_number=$(echo "$contact" | cut -d ',' -f 2)
 
                 read -p "Enter the new contact name: " new_name
 
-                sed -i "s/^$name,$current_name,$current_number/$name,$new_name,$current_number/" "$myfile"
+                sed -i '' "s/^$current_name,$current_number/$new_name,$current_number/" "$myfile"
                 echo "Contact name updated successfully." 
                 ;;
 
 
             2)
-                current_name=$(echo "$contact" | cut -d ',' -f 2)
-                current_number=$(echo "$contact" | cut -d ',' -f 3)
+                current_name=$(echo "$contact" | cut -d ',' -f 1)
+                current_number=$(echo "$contact" | cut -d ',' -f 2)
 
                 read -p "Enter the new number: " new_number
 
-                sed -i "s/^$name,$current_name,$current_number/$name,$current_name,$new_number/" "$myfile"
+                sed -i '' "s/^$current_name,$current_number/$current_name,$new_number/" "$myfile"
                 echo "Price updated successfully." 
                 ;;
 
              3)
-                current_name=$(echo "$contact" | cut -d ',' -f 2)
-                current_number=$(echo "$contact" | cut -d ',' -f 3)
+                current_name=$(echo "$contact" | cut -d ',' -f 1)
+                current_number=$(echo "$contact" | cut -d ',' -f 2)
 
                 read -p "Enter the new contact name: " new_name
                 read -p "Enter the new number: " new_number
 
-                sed -i "s/^$name,$current_name,$current_number/$name,$new_name,$new_number/" "$myfile"
+                sed -i '' "s/^$current_name,$current_number/$new_name,$new_number/" "$myfile"
                 # sed -i "s/^$id,[^,]*,[^,]*/$id,$new_name,$new_price/" "$myfile" 
 
                 echo "Contact name and number updated successfully." 
