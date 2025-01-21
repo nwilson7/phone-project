65f279932567d52c7fbf20814d5e72013cb02ab8
mirunaaf <filfan.miruna@gmsil.com>
2025-01-21 14:34:11 +0000

    searchContacts function

---
+++ searchContacts.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+myfile="contacts.txt"
+
+searchContacts()
+{
+        read -p "Enter 1 for search by name 2 search by mobile number: " ch
+
+        case $ch in
+                1) read -p "Enter contact name you want to search for: " name
+                if [ ! "$myfile" ]; then
+                        echo "File = $myfile doesn't exist."
+                else
+
+                        if grep -q "^$name," "$myfile"; then
+                                grep "^$name," "$myfile"
+
+                        else
+                                echo "contact with name = $name doesn't exist"
+                        fi
+                fi ;;
+
+                2) read -p "Enter mobile number you want to search for: " number
+                if [ ! "$myfile" ]; then
+                        echo "File = $myfile doesn't exist."
+                else
+
+                        if grep -iq "$number," "$myfile"; then
+                                grep -i "$number," "$myfile"
+                        else
+                                echo "Contact with mobile number = $number doesn't exist"
+                        fi
+                fi ;;
+
+                *) echo "invalid choice" ;;
+        esac
+
+}
+
+searchContacts
