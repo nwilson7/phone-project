3f293ee6dbc61a9f2e0f42ee57bc23d704a32b0b
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2025-01-21 12:16:15 +0000

    adds functionality to addContacts

--- addContacts.sh
+++ addContacts.sh
@@ -4,20 +4,21 @@ myfile="Contacts.txt"
 
 if [ ! -f "$myfile" ]; then
         touch "$myfile"
-        echo "NAME,MNUMBER" > "$myfile"
+        echo "NAME,MNUMBER" >"$myfile"
 fi
 
-add_Contacts()
-{
-    read -p "Enter the name = " name
-        if grep -q "^$name," "$myfile" ;then
+add_Contacts() {
+        read -p "Enter the contact's name = " name
+        if grep -q "^$name," "$myfile"; then
                 echo "id = $name already exists"
                 return
         fi
-        read -p "Enter the mnumber = " mnumber
-        if grep -q ",$pname," "$myfile" ;then
-                echo "productname = $pname already exists"
+        read -p "Enter the mobile number = " mnumber
+        if grep -q ",$mnumber," "$myfile"; then
+                echo "Mobile Number = $mnumber already exists"
                 return
         fi
-
+        echo "$name,$mnumber" >>"$myfile"
 }
+
+add_Contacts
